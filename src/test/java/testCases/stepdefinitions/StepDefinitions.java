2b803775381aeb2d55b9766ddb11c3eb8676d925
Sumeet Bandgar <sumeet.bandgar@verinite.com>
2024-09-03 12:28:14 +0530

    Commit for Project0.6584358542565509

--- src/test/java/testCases/stepdefinitions/StepDefinitions.java
+++ src/test/java/testCases/stepdefinitions/StepDefinitions.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import Utils.PropertyFileReader;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -17,61 +16,67 @@ import pages.demosite.Screen;
 
 public class StepDefinitions extends UIInteractions {
 
-	private static final Logger logger = LoggerFactory.getLogger(StepDefinitions.class);
+    private static final Logger logger = LoggerFactory.getLogger(StepDefinitions.class);
 
-	PropertyFileReader prop = new PropertyFileReader("src/test/resources/config.properties");
+    PropertyFileReader prop = new PropertyFileReader("src/test/resources/config.properties");
 
-	@Steps
-	Screen screen;
+    @Steps
+    Screen screen;
 
-	@Given("{} is on {} page")
-	public void userIsOnUrlPage(String user, String url) {
-		screen.open(user, url);
-	}
+    @Given("{} is on {} page")
+    public void userIsOnUrlPage(String user, String url) {
+        System.out.println("parameters: " + user + ", " + url);
+        screen.open(elementType, elementKey);
+    }
 
-	@When("he views the value in the field {}")
-	public void heViewsTheValueInTheFieldXpath(String xPath) {
-		System.out.println("parameters: " + xPath);
+    @When("he views the value in the field {}")
+    public void heViewsTheValueInTheFieldXpath(String xPath) {
+        System.out.println("parameters: " + xPath);
+    }
 
-	}
+    @Then("he should be able to view {} in the field")
+    public void heShouldBeAbleToViewDataInTheField(String data) {
+        System.out.println("parameters: " + data);
+        wait.until(ExpectedConditions.titleContains(data));
+        logger.info("Page title contains: {}", data);
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 
-	@Then("he should be able to view {} in the field")
-	public void heShouldBeAbleToViewDataInTheField(String data) {
-		screen.view(data);
-	}
+    @When("he enters the value {} in the field with a path {}")
+    public void heEntersTheValueDataInTheFieldWithAPathXpath(String data, String xPath) {
+        System.out.println("parameters: " + data + ", " + xPath);
+    }
 
-	@When("he enters the value {} in the field with a path {}")
-	public void heEntersTheValueDataInTheFieldWithAPathXpath(String data, String xPath) {
-		System.out.println("parameters: " + data + ", " + xPath);
+    @When("he enters the value {} in the field identified by {}")
+    public void heEntersTheValueDataInTheFieldIdentifiedByElementId(String data, String elementId) {
+        System.out.println("parameters: " + data + ", " + elementId);
+    }
 
-	}
+    @When("he enters the value {} in the field named {}")
+    public void heEntersTheValueDataInTheFieldNamedElementName(String data, String elementName) {
+        System.out.println("parameters: " + data + ", " + elementName);
+        WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name(elementName)));
+        searchBox.clear();
+        searchBox.sendKeys(data);
+    }
 
-	@When("he enters the value {} in the field identified by {}")
-	public void heEntersTheValueDataInTheFieldIdentifiedByElementId(String data, String elementId) {
-		System.out.println("parameters: " + data + ", " + elementId);
+    @When("he clicks the button identified by {}")
+    public void heClicksTheButtonIdentifiedByButtonId(String buttonId) {
+        System.out.println("parameters: " + buttonId);
+    }
 
-	}
+    @When("he clicks the button named {}")
+    public void heClicksTheButtonNamedButtonName(String buttonName) {
+        System.out.println("parameters: " + buttonName);
+        WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.name(buttonName)));
+        searchButton.click();
+        logger.info("Clicked the button: {}", buttonName);
+    }
 
-	@When("he enters the value {} in the field named {}")
-	public void heEntersTheValueDataInTheFieldNamedElementName(String data, String elementName) throws Exception {
-		screen.updateByElementName(data, elementName);
-	}
-
-	@When("he clicks the button identified by {}")
-	public void heClicksTheButtonIdentifiedByButtonId(String buttonId) {
-		System.out.println("parameters: " + buttonId);
-
-	}
-
-	@When("he clicks the button named {}")
-	public void heClicksTheButtonNamedButtonName(String buttonName) throws Exception {
-		screen.click(buttonName);
-	}
-
-	@When("he clicks the element identified by {}")
-	public void heClicksTheElementIdentifiedByElementId(String elementId) {
-		System.out.println("parameters: " + elementId);
-
-	}
-
+    @When("he clicks the element identified by {}")
+    public void heClicksTheElementIdentifiedByElementId(String elementId) {
+        System.out.println("parameters: " + elementId);
+    }
 }
